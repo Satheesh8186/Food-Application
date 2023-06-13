ccca6039f6b92c926ae8df6f8c019f42d43e493c
satheesh <sknaidu8186@gmail.com>
2023-06-12 22:25:16 -0400

    Initial Commit

---
+++ Food Application/Food_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_ApplicationApp.swift
+//  Food Application
+//
+//  Created by satheesh kumar on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
